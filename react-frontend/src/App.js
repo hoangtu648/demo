9a563202453e5e982328031c4df2db6fd22e8811
hoangtu648 <tuhoangnguyen2003@gmail.com>
2023-07-16 21:30:27 +0700

    init project

---
+++ react-frontend/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import logo from './logo.svg';
+import ListEmployeeComponent from './components/ListEmployeeComponent';
+
+function App() {
+  return (
+    <div className="container">
+      <ListEmployeeComponent />
+    </div>
+  );
+}
+
+export default App;
